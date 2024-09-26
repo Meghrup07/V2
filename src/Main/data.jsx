e0506da6e4821af2a4b7acb0dacf98a970b24ebc
Meghshyam Wagh <65434438+Meghrup07@users.noreply.github.com>
2024-09-26 17:24:17 +0530

    change and update experience descriptions

--- src/Main/data.jsx
+++ src/Main/data.jsx
@@ -16,20 +16,23 @@ export const dataExperience = {
     0: {
       title: "IndoWorld Consulting & Technology Services Pvt. Ltd. 🏢",
       date: "Aug 2023 - Present",
-      first: "Developed React applications using Redux Toolkit state management and RESTful APIs. Developed RESTful APIs using ASP.NET Core and Django Rest Framework for complex data-driven applications. Developed and maintained web applications using Django, HTML5, CSS3, JavaScript, jQuery and MySQL.",
+      first: "Developed RESTful APIs using ASP.NET Core Web API for complex applications",
+      second: "Designed and maintained web applications using Angular HTML5, CSS3, TypeScript and jQuery.",
+      third: "Developed React applications using Redux Toolkit state management and RESTful APIs.",
     },
     1: {
       title: "ekZero Enterprise Software Services Pvt. Ltd. 🏢",
       date: "Sept 2022 - Aug 2023",
-      first: "Contributed to Frontend Development, proficiently working with technologies such as HTML5, CSS3, SCSS, jQuery, Bootstrap and Angular. Successfully integrated REST APIs within the Angular framework during the tenure. Collaborated with backend developers to integrate RESTful APIs into the application.",
+      first: "Contributed to Frontend Development, proficiently working with technologies such as HTML5, CSS3, SCSS, jQuery, Bootstrap and Angular.",
+      second: "Successfully integrated REST APIs within the Angular framework during the tenure.",
+      third: "Collaborated with backend developers to integrate RESTful APIs into the application.",
     },
     2: {
       title: "Helios Solutions 🏢",
       date: "Dec 2020 - Aug 2022",
-      first: "Worked on Web Development..",
-      second: "Design and Develop Web Sites and Web Applictions.",
-      third:
-        "Worked on Technologies like HTMl5, CSS3, jQuery, JavaScript, Angular, Sass and Bootstrap.",
+      first: "Worked on Web Development. Design and Develop Websites and Web applications.",
+      second: "Rest API integration for Frontend. Worked on Technologies like HTMl5, CSS3, jQuery, JavaScript and Angular.",
+      third:"Developed user-friendly and visually appealing web interfaces using HTML5, CSS3, SCSS, Bootstrap and JavaScript.",
     },
   },
 };
