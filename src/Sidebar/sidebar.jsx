2a74023e48053aff9a70a0eee62c1198963514d3
Meghshyam Wagh <65434438+Meghrup07@users.noreply.github.com>
2025-03-13 15:49:11 +0530

    Resume link Changed

--- src/Sidebar/sidebar.jsx
+++ src/Sidebar/sidebar.jsx
@@ -85,7 +85,7 @@ const Sidebar = ({ handleToast }) => {
       <LeftSideContainer>
         <SideIcon whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.85 }}>
           <a
-            href="https://drive.google.com/file/d/1tegZthMMiNowKkoCGayZ-be8GquaTuE1/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Bgr42bTw_jNXPe7myszJlZ-aEFjhW6ZP/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer">
             <FilePdf />
