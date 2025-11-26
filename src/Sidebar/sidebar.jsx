76091fd1ea775b79f4731e57a650a2544eeb0be1
Meghshyam Wagh <65434438+Meghrup07@users.noreply.github.com>
2025-11-26 10:52:44 +0530

    Update sidebar link to new Google Drive URL

--- src/Sidebar/sidebar.jsx
+++ src/Sidebar/sidebar.jsx
@@ -85,7 +85,7 @@ const Sidebar = ({ handleToast }) => {
       <LeftSideContainer>
         <SideIcon whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.85 }}>
           <a
-            href="https://drive.google.com/file/d/1QKLYfCnKgca4F9nmKq90zMLyWvVUkH5h/view?usp=sharing"
+            href="hhttps://drive.google.com/file/d/16sVir45J7osIKYS2ldmMbMGRa9EaWNTM/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer">
             <FilePdf />
